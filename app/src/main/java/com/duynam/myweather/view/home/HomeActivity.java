daa935f33a93fed404c2a64ccee4840dd402a245
duynam98 <duynam6262@gmail.com>
2020-03-09 15:51:44 +0700

    layout Home + request permission location

--- app/src/main/java/com/duynam/myweather/view/home/HomeActivity.java
+++ app/src/main/java/com/duynam/myweather/view/home/HomeActivity.java
@@ -1,16 +1,73 @@
 package com.duynam.myweather.view.home;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Point;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import com.duynam.myweather.R;
+import com.duynam.myweather.databinding.ActivityHomeBinding;
+import com.duynam.myweather.utils.Constant;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private ActivityHomeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        initDataBinding();
+        setHeightContainer1();
+        initPermission();
     }
+
+    private void initDataBinding() {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_home);
+    }
+
+    private void setHeightContainer1() {
+        Point size = new Point();
+        getWindowManager().getDefaultDisplay().getSize(size);
+        int screenHeight = size.y;
+        ViewGroup.LayoutParams params = binding.container1.getLayoutParams();
+        params.height = screenHeight;
+    }
+
+    private void initPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                    checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                if (!shouldShowRequestPermissionRationale(Manifest.permission.READ_CONTACTS)) {
+                    requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, Constant.REQUEST_CODE);
+                }
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, Constant.REQUEST_CODE);
+                return;
+            }
+            Toast.makeText(this, "Có quyền rồi", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case Constant.REQUEST_CODE:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Có quyền rồi", Toast.LENGTH_SHORT).show();
+                } else {
+
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
 }
